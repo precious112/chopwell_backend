c2d59c79799052af52e55b93a989bd628862e725
precious112 <preciouskent8@gmail.com>
2022-06-23 13:44:15 -0700

    new

--- chopwell_backend/settings.py
+++ chopwell_backend/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = str(os.environ.get('DEBUG'))=="1"
-DEBUG=True
+DEBUG = str(os.environ.get('DEBUG'))=="1"
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','chopwell.herokuapp.com']
 
