031ec832ac114f1d99cec30cbf507731ea2c3bd3
precious112 <preciouskent8@gmail.com>
2022-06-23 12:22:23 -0700

    new

--- chopwell_backend/settings.py
+++ chopwell_backend/settings.py
@@ -33,9 +33,20 @@ DEBUG = str(os.environ.get('DEBUG'))=="1"
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','chopwell.herokuapp.com']
 
-'''GDAL_LIBRARY_PATH= 'C:\\OSGeo4W\\bin\\gdal304.dll'
+GDAL_LIBRARY_PATH= 'C:\\OSGeo4W\\bin\\gdal304.dll'
 GEOS_LIBRARY_PATH= 'C:\\OSGeo4W\\bin\\geos_c.dll'
-GDAL_DATA='C:\\OSGeo4W\\share\\gdal' '''
+GDAL_DATA='C:\\OSGeo4W\\share\\gdal' 
+
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    '''#if '64' in platform.architecture()[0]:
+        OSGEO4W += "64" '''
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH'] 
 
 
 
