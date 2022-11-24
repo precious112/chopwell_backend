739e15d5c46bcad37b217ca0fa38f11a54b405bc
precious112 <preciouskent8@gmail.com>
2022-11-24 01:42:56 -0800

    updated static files directory

--- chopwell_backend/settings.py
+++ chopwell_backend/settings.py
@@ -205,9 +205,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-#STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 
 
